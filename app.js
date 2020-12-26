d6829a381fd8d3bc858318860b0fe9e757a15791
Doron Michlal Raz <68538636+DoronMichlal@users.noreply.github.com>
2020-12-26 21:55:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,39 +8,39 @@ const selectElement = document.querySelector('#select');
 // Grabbing the form and listening for submit
 const form = document.querySelector('#form');
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const searchValue = document.querySelector('#search').value;
+	e.preventDefault();
+	const searchValue = document.querySelector('#search').value;
 
-    music
-        .getData(searchValue)
-        .then((data) => ui.turnDataToArray(data))
-        .then((arr) => ui.render(arr))
-        .then(ui.resetForm())
-        .catch((err) => console.log(err));
+	music
+		.getData(searchValue)
+		.then(data => ui.turnDataToArray(data))
+		.then(arr => ui.render(arr))
+		.then(ui.resetForm())
+		.catch(err => console.log(err));
 });
 
 // Make "press enter" message gone after 5 seconds
 setTimeout(() => {
-    document.getElementById('small').style.display = 'none';
+	document.getElementById('small').style.display = 'none';
 }, 5000);
 
 // Select 'change' event
-selectElement.addEventListener('change', (event) => {
-    const searchValue = document.querySelector('#search').value;
+selectElement.addEventListener('change', event => {
+	const searchValue = document.querySelector('#search').value;
 
-    if (event.target.value === '') {
-        music
-            .getData(searchValue)
-            .then((data) => ui.turnDataToArray(data))
-            .then((arr) => ui.render(arr))
-            .then(ui.resetForm())
-            .catch((err) => console.log(err));
-    } else {
-        music
-            .getData(searchValue)
-            .then((data) => ui.turnDataToArray(data))
-            .then((arr) => ui.sortArrayOfObjects(arr, event.target.value))
-            .then((sortedData) => ui.render(sortedData))
-            .catch((err) => console.log(err));
-    }
+	if (event.target.value === '') {
+		music
+			.getData(searchValue)
+			.then(data => ui.turnDataToArray(data))
+			.then(arr => ui.render(arr))
+			.then(ui.resetForm())
+			.catch(err => console.log(err));
+	} else {
+		music
+			.getData(searchValue)
+			.then(data => ui.turnDataToArray(data))
+			.then(arr => ui.sortArrayOfObjects(arr, event.target.value))
+			.then(sortedData => ui.render(sortedData))
+			.catch(err => console.log(err));
+	}
 });
